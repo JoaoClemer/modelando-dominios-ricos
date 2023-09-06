1c75e26b05839149d837b92240367c38fb4d5528
João Clemer <joaoclemerlima@gmail.com>
2023-09-05 21:21:46 -0300

    Entities

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,19 @@
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+
+        public void AddSubscription()
+        {
+            var subscription = new Subscription(DateTime.Now.AddMonths(1));
+            var student = new Student("João", "Clemer", "12345678912", "joaoclemerlima@gmail.com");
+            
+            student.AddSubscription(subscription);
+
+        }
+    }
+}
